ebb9fe2eeabe6624020481bf45e2f1750e19adfe
POWER-WORLD <pk3253702@gmail.com>
2025-11-02 22:41:24 +0530

    first commit

--- client/src/services/projectService.js
+++ client/src/services/projectService.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:5000/api';
+import api from './api';
 
 export const getAllProjects = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/projects`);
+        const response = await api.get(`/projects`);
         console.log("Fetched projects:", response.data);
         return response.data;
     } catch (error) {
@@ -15,7 +13,7 @@ export const getAllProjects = async () => {
 
 export const addProject = async (projectData) => {
     try {
-        const response = await axios.post(`${API_BASE_URL}/projects`, projectData);
+        const response = await api.post(`/projects`, projectData);
         return response.data;
     } catch (error) {
         console.error('Error adding project to database:', error);
@@ -25,7 +23,7 @@ export const addProject = async (projectData) => {
 
 export const updateProject = async (id, projectData) => {
   try {
-    const response = await axios.put(`${API_BASE_URL}/projects/${id}`, projectData);
+    const response = await api.put(`/projects/${id}`, projectData);
     return response.data;
   } catch (error) {
     throw error;
@@ -34,7 +32,7 @@ export const updateProject = async (id, projectData) => {
 
 export const deleteProject = async (id) => {
     try {
-        const response = await axios.delete(`${API_BASE_URL}/projects/${id}`);
+        const response = await api.delete(`/projects/${id}`);
         return response.data;
     } catch (error) {
         console.error('Error deleting project from database:', error);
