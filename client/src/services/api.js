00b7e2883e6a43aaa5b78c5ceee7915600112b18
POWER-WORLD <pk3253702@gmail.com>
2025-11-03 00:00:41 +0530

    first commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://resume-app-web-production.up.railway.app/api", // backend base URL
+  baseURL: "https://resume-app-webserver-production.up.railway.app/api", // backend base URL
 });
 
 export default api;
